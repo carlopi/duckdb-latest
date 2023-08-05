c56fd49f06bdc24d9dbdcedd1ad3c0e859b9fc08
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-08-05 12:09:03 +0200

    Fix commit message

--- collect.sh
+++ collect.sh
@@ -1,3 +1,6 @@
 gh run download $(gh run list -L 7 -R duckdb/duckdb -e repository_dispatch -w Windows --json databaseId | jq "[.[]][$DAYS].databaseId") -R duckdb/duckdb --dir collect
 gh run download $(gh run list -L 7 -R duckdb/duckdb -e repository_dispatch -w OSX --json databaseId | jq "[.[]][$DAYS].databaseId") -R duckdb/duckdb --dir collect
 gh run download $(gh run list -L 7 -R duckdb/duckdb -e repository_dispatch -w LinuxRelease --json databaseId | jq "[.[]][$DAYS].databaseId") -R duckdb/duckdb --dir collect
+gh run list -L 7 -R duckdb/duckdb -e repository_dispatch -w LinuxRelease --json headSha | jq "[.[]][0].headSha" > hash
+HASHED_VALUE=$(cat hash | head -c 9 | tail -c 8)
+echo "HASH=$HASHED_VALUE" >> $GITHUB_ENV
