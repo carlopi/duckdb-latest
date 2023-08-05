a6852636a0cc3e27ea4c03d1e716f77618499e05
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-08-05 11:56:30 +0200

    Customize how many days back

--- collect.sh
+++ collect.sh
@@ -1,3 +1,3 @@
-gh run download $(gh run list -L 3 -R duckdb/duckdb -e repository_dispatch -w Windows --json databaseId | jq "[.[]][1].databaseId") -R duckdb/duckdb --dir collect
-gh run download $(gh run list -L 3 -R duckdb/duckdb -e repository_dispatch -w OSX --json databaseId | jq "[.[]][1].databaseId") -R duckdb/duckdb --dir collect
-gh run download $(gh run list -L 3 -R duckdb/duckdb -e repository_dispatch -w LinuxRelease --json databaseId | jq "[.[]][1].databaseId") -R duckdb/duckdb --dir collect
+gh run download $(gh run list -L 7 -R duckdb/duckdb -e repository_dispatch -w Windows --json databaseId | jq "[.[]][$DAYS].databaseId") -R duckdb/duckdb --dir collect
+gh run download $(gh run list -L 7 -R duckdb/duckdb -e repository_dispatch -w OSX --json databaseId | jq "[.[]][$DAYS].databaseId") -R duckdb/duckdb --dir collect
+gh run download $(gh run list -L 7 -R duckdb/duckdb -e repository_dispatch -w LinuxRelease --json databaseId | jq "[.[]][$DAYS].databaseId") -R duckdb/duckdb --dir collect
