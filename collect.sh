f317383649bab1b3d889d021e3e5744643e1fe0f
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-05-18 10:10:35 +0200

    Update collect.sh to only do v1.3-ossivalis

--- collect.sh
+++ collect.sh
@@ -1,4 +1,4 @@
-gh run download $(gh run list -L 20 -R duckdb/duckdb -e repository_dispatch -w InvokeCI --json databaseId | jq "[.[]][$DAYS].databaseId") -R duckdb/duckdb --dir collect
+gh run download $(gh run list -L 20 -R duckdb/duckdb -e workflow_dispatch -w InvokeCI --branch v1.3-ossivalis --json databaseId | jq "[.[]][$DAYS].databaseId") -R duckdb/duckdb --dir collect
 gh run list -L 7 -R duckdb/duckdb -e repository_dispatch -w InvokeCI --json headSha | jq "[.[]][0].headSha" > hash
 HASHED_VALUE=$(cat hash | head -c 9 | tail -c 8)
 echo "HASH=$HASHED_VALUE" >> $GITHUB_ENV
